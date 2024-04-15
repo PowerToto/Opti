6d75f23edb40837afb858eddac0ee79ed2bcaf86
WIECZOREK Thomas <thomas.wieczorek1@etu.univ-lorraine.fr>
2024-04-15 23:15:32 +0200

    jsp ce que je fais

--- DonneesCodePizza/AlgoGenetique.py
+++ DonneesCodePizza/AlgoGenetique.py
@@ -7,7 +7,7 @@ except:
     raise Exception("Erreur à la lecture des arguments")
 
 
-iterations = 40
+iterations = 200
 data = []
 try:
     with open(instance_file, "r") as f:
@@ -59,21 +59,23 @@ def createFile(ingr):
 def algorithme_genetique(taille_population, prob_mut, max_iterations):
     population = []
     for _ in range(taille_population):
-        population.append(generer_pizza())
+        pizz = generer_pizza()
+        while pizz in population:
+            pizz = generer_pizza()
+        population.append(pizz)
     max_score = 0
     while max_iterations != 0:
         # Évaluation de la population
         for pizza in population:
             createFile(pizza)
-            res = subprocess.getoutput("python3 evaluation.py "+str(instance_file)+ " "+"solution.txt")
+            res = subprocess.getoutput("python3 evaluation.py " + str(instance_file) + " solution.txt")
             score = int(res.split()[-1])
             if score > max_score:
                 max_iterations = iterations
-                meilleure_pizza, max_score = pizza,score
-                print(str(pizza)+" "+str(score))
-
+                meilleure_pizza, max_score = pizza, score
+                print(str(pizza) + " " + str(score))
             else:
-                max_iterations = max_iterations-1
+                max_iterations = max_iterations - 1
 
         # Sélection des parents pour le croisement
         parents = [pizza for pizza in population[:taille_population // 2]]
@@ -87,10 +89,14 @@ def algorithme_genetique(taille_population, prob_mut, max_iterations):
                 enfant1 = mutation(enfant1)
             if random.random() < prob_mut:
                 enfant2 = mutation(enfant2)
-            nouvelle_population.extend([enfant1, enfant2])
+            #on retire les doublons et on retrie a chaque fois pour verifier les doublons potentiels
+            if enfant1 not in nouvelle_population:  
+                nouvelle_population.append(enfant1)
+            if enfant2 not in nouvelle_population:  
+                nouvelle_population.append(enfant2)
         population = nouvelle_population
     return meilleure_pizza, max_score
 
-meilleure_pizza, meilleur_score = algorithme_genetique(taille_population=100, prob_mut=0.05, max_iterations=iterations)
+meilleure_pizza, meilleur_score = algorithme_genetique(taille_population=40, prob_mut=0.05, max_iterations=iterations)
 print("Meilleure pizza trouvée:", meilleure_pizza)
 print("Score obtenu:", meilleur_score)
