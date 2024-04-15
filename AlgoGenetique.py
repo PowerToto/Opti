62fdeb9d000ef4d6bc24b4491edfe347a95dde5a
WIECZOREK Thomas <thomas.wieczorek1@etu.univ-lorraine.fr>
2024-04-15 18:47:09 +0200

    debut algo génétique

---
+++ AlgoGenetique.py
@@ -0,0 +1,92 @@
+import sys
+import subprocess
+import random
+try:
+    instance_file = sys.argv[1]
+except:
+    raise Exception("Erreur à la lecture des arguments")
+
+data = []
+try:
+    with open(instance_file, "r") as f:
+        data = f.readlines()
+    data = [l.strip().split() for l in data]
+except:
+    raise Exception("Erreur lors de la lecture de l'instance")
+
+Nclients = int(data[0][0]) # Nombre total de clients
+data.pop(0)
+
+ingredients = dict() # nom d'un ingrédient (str) -> identifiant (entier allant de 0 à N-1)
+noms_ingredients = [] # identifiant (entier allant de 0 à N-1, indice dans la liste) -> nom de l'ingrédient (str) qui a cet identifiant
+
+Ningredients = 0
+
+for client in range(Nclients):
+    Lc,Dc = data[2*client][1:], data[2*client+1][1:] # préférences du client
+    for nom_ingr in Lc + Dc:
+        if nom_ingr not in ingredients: # nom_ingr n'est pas dans les clés du dictionnaire -> c'est un ingrédient que l'on a pas encore rencontré
+            ingredients[nom_ingr] = Ningredients # on lui attribue un numéro unique dans [0;N-1]
+            noms_ingredients.append(nom_ingr)
+            Ningredients += 1 
+
+def generer_pizza():
+    return random.choice(ingredients)
+
+def croisement (pizza1,pizza2):
+    point = random.randint(1, len(ingredients) - 1)
+    new_pizza1 = pizza1[:point]+pizza2[point:]
+    new_pizza2 = pizza2[:point]+pizza1[point:]
+    return new_pizza1,new_pizza2
+
+def mutation(pizza):
+    nouvelle_pizza = pizza[:]
+    new_ing = random.choice(ingredients)
+    while new_ing in nouvelle_pizza:
+        new_ing = random.choice(ingredients)
+    nouvelle_pizza.append(new_ing)
+    return nouvelle_pizza
+
+def createFile(ingr):
+    with open("solution.txt", "w") as f:
+        f.write(str(len(ingr))+" ")
+        for i in ingr:
+            f.write(i+" ")
+
+def algorithme_genetique(taille_population, prob_mut, max_iterations):
+    population = [generer_pizza() for _ in range(taille_population)]
+    max_score = 0
+    for i in range(max_iterations):
+        # Évaluation de la population
+        for pizza in population:
+            createFile(pizza)
+            res = subprocess.getoutput("python3 evaluation.py "+str(instance_file)+ " "+"solution.txt")
+            score = int(res.split()[-1])
+            if score > max_score:
+                meilleure_pizza, max_score = pizza,score
+      
+        # Critère d'arrêt si on atteint le score maximal
+        if max_score >= 1750:
+            break
+
+        # Sélection des parents pour le croisement
+        parents = [pizza for pizza in population[:taille_population / 2]]
+
+        # Croisement et mutation
+        nouvelle_population = []
+        while len(nouvelle_population) < taille_population:
+            parent1, parent2 = random.sample(parents, 2)
+            enfant1, enfant2 = croisement(parent1, parent2)
+            if random.random() < prob_mut:
+                enfant1 = mutation(enfant1)
+            if random.random() < prob_mut:
+                enfant2 = mutation(enfant2)
+            nouvelle_population.extend([enfant1, enfant2])
+
+        population = nouvelle_population
+
+    return meilleure_pizza, max_score
+
+meilleure_pizza, meilleur_score = algorithme_genetique(taille_population=50, prob_mut=0.5, max_iterations=10)
+print("Meilleure pizza trouvée:", meilleure_pizza)
+print("Score obtenu:", meilleur_score)
